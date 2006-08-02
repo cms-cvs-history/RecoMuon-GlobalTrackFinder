54c7cf991870abe3f6516549c35523fbce66cb73
Adam Everett <sha1-0480fa7b9471174cc688a8c2e992d84c4f17ed6d@cern.ch>
2006-08-02 21:53:41 +0000

    fixes

--- test/GLBTrackMatchAnalyzer.cc
+++ test/GLBTrackMatchAnalyzer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Adam Everett
 //         Created:  Thu Jul 20 23:15:21 CEST 2006
-// $Id$
+// $Id: GLBTrackMatchAnalyzer.cc,v 1.1 2006/07/26 23:58:24 aeverett Exp $
 //
 //
 
@@ -36,9 +36,14 @@
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "RecoMuon/TrackingTools/interface/MuonPatternRecoDumper.h"
 
 #include "RecoMuon/GlobalTrackFinder/interface/GlobalMuonTrackMatcher.h"
+#include "RecoMuon/TrackingTools/interface/MuonUpdatorAtVertex.h"
+
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
 #include "TFile.h"
 #include "TH1F.h"
@@ -51,31 +56,32 @@ using namespace edm;
 //
 
 class GLBTrackMatchAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit GLBTrackMatchAnalyzer(const edm::ParameterSet&);
-      ~GLBTrackMatchAnalyzer();
-
-
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-   private:
-      // ----------member data ---------------------------
+public:
+  explicit GLBTrackMatchAnalyzer(const edm::ParameterSet&);
+  ~GLBTrackMatchAnalyzer();
+   
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+private:
+  // ----------member data ---------------------------
   std::string theRootFileName;
   TFile* theFile;
-
+  
+  GlobalMuonTrackMatcher* theTrackMatcher;
+  edm::ESHandle<MagneticField> theMGField;
+  
   std::string theSTAMuonLabel;
   std::string theTkTracksLabel;
-  std::string theSeedCollectionLabel;
   std::string theDataType;
-
+  
   double theTrackMatcherChi2Cut;
-
+  
   // Histograms
   TH1F *hChi2;
-
+  
   // Counters
-  int numberOfSimTracks;
-  int numberOfRecTracks;
-
+  int numberOfTkTracks;
+  int numberOfStaTracks;
+  
 };
 
 //
@@ -91,41 +97,41 @@ class GLBTrackMatchAnalyzer : public edm::EDAnalyzer {
 //
 GLBTrackMatchAnalyzer::GLBTrackMatchAnalyzer(const edm::ParameterSet& iConfig)
 {
-   //now do what ever initialization is needed
-  
-  theSTAMuonLabel = iConfig.getUntrackedParameter<string>("StandAloneTrackCollectionLabel");
-  theTkTracksLabel = iConfig.getUntrackedParameter<string>("TkTracksCollectionLabel");
-  theSeedCollectionLabel = iConfig.getUntrackedParameter<string>("MuonSeedCollectionLabel");
-
+  numberOfStaTracks = 0;
+  numberOfTkTracks = 0;
+  //now do what ever initialization is needed
+  theSTAMuonLabel = iConfig.getUntrackedParameter<string>("MuonCollectionLabel");
+  theTkTracksLabel = iConfig.getUntrackedParameter<string>("TkTrackCollectionLabel");
   theRootFileName = iConfig.getUntrackedParameter<string>("rootFileName");
-
   theDataType = iConfig.getUntrackedParameter<string>("DataType");
   theTrackMatcherChi2Cut = iConfig.getParameter<double>("Chi2CutTrackMatcher");
   
+  theTrackMatcher = new GlobalMuonTrackMatcher(theTrackMatcherChi2Cut);
+  
   if(theDataType != "RealData" && theDataType != "SimData")
     cout<<"Error in Data Type!!"<<endl;
-
+  
   // Create the root file
   theFile = new TFile(theRootFileName.c_str(), "RECREATE");
   theFile->cd();
-
-  hChi2 = new TH1F("trackChi2","Track Chi2",100,-2,2);
+  
+  hChi2 = new TH1F("trackChi2","Track Chi2",10000,0,100000);
   
 }
 
 
 GLBTrackMatchAnalyzer::~GLBTrackMatchAnalyzer()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
+  
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
   
   if(theDataType == "SimData"){
-    cout << endl << endl << "Number of Sim tracks: " << numberOfSimTracks << endl;
+    cout << endl << endl << "Number of Events with STA tracks: " << numberOfStaTracks << endl;
   }
-
-  cout << "Number of Reco tracks: " << numberOfRecTracks << endl << endl;
-    
+  
+  cout << "Number of Events with Tk tracks: " << numberOfTkTracks << endl << endl;
+  cout << "Number of histogram entries: " << hChi2->GetEntries() << endl << endl;
   // Write the histos to file
   theFile->cd();
   hChi2->Write();
@@ -140,45 +146,52 @@ GLBTrackMatchAnalyzer::~GLBTrackMatchAnalyzer()
 
 // ------------ method called to produce the data  ------------
 void
-GLBTrackMatchAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& eventSetup)
+GLBTrackMatchAnalyzer::analyze(const edm::Event& event, 
+			       const edm::EventSetup& eventSetup)
 {
   //using namespace edm;
-  
-  //cout << "Run: " << event.id().run() << " Event: " << event.id().event() << endl;
-  
-  MuonPatternRecoDumper debug;
+  LogInfo("TrackMatchTestAnalyzer")
+    << "***** Run: " << event.id().run() 
+    << " Event: " << event.id().event() << endl;
   
   // Get the RecTrack collection from the event
-  Handle<reco::TrackCollection> staTracks;
+  edm::Handle<reco::TrackCollection> staTracks;
   event.getByLabel(theSTAMuonLabel, staTracks);
-
-  Handle<reco::TrackCollection> tkTracks;
+  
+  edm::Handle<reco::TrackCollection> tkTracks;
   event.getByLabel(theTkTracksLabel, tkTracks);
-
-  ESHandle<MagneticField> theMGField;
+  
   eventSetup.get<IdealMagneticFieldRecord>().get(theMGField);
 
-  //GlobalMuonTrackMatcher* theTrackMatcher = new GlobalMuonTrackMatcher(theTrackMatcherChi2Cut,&*theMGField,theUpdator);
-  GlobalMuonTrackMatcher* theTrackMatcher = new GlobalMuonTrackMatcher(theTrackMatcherChi2Cut);
+  theTrackMatcher->setES(eventSetup);
 
-  //reco::TrackCollection::const_iterator staTrack;
- 
-  std::pair<bool, reco::TrackRef> bestMatch;
-  std::pair<bool, double> bestChi2;
- 
-  cout<<"Reconstructed STAMuon tracks: " << staTracks->size() << endl;
-  cout<<"Reconstructed Tracker tracks: " << tkTracks->size() << endl;
+  LogInfo("GLBTrackMatchAnalyzer")
+    <<"Reconstructed STAMuon tracks: " 
+    << staTracks->size() << "\n";
+  LogInfo("GLBTrackMatchAnalyzer")
+    <<"Reconstructed Tracker tracks: " 
+    << tkTracks->size() << "\n";
+  if(tkTracks->size() >= 1) {numberOfTkTracks++;}
+  if(staTracks->size() >= 1) {numberOfStaTracks++;}
   for (unsigned int position = 0; position != staTracks->size(); ++position) {
-    LogDebug("TrackMatchTest")<<"+++ New Track +++"<<endl;
+    //LogInfo("GLBTrackMatchAnalyzer")
+    //<<"+++ New Track +++ "<< position <<endl;
+    
     reco::TrackRef staTrack(staTracks,position);
-    bestMatch = theTrackMatcher->matchOne(staTrack,tkTracks);
+    
+    //LogInfo("GLBTrackMatchAnalyzer")
+    //<<"staTrack "<< staTrack->momentum() <<endl;
+    std::pair<bool, reco::TrackRef> bestMatch = 
+      theTrackMatcher->matchOne(staTrack,tkTracks);
+    
     if(bestMatch.first){
-      bestChi2 = theTrackMatcher->match(*staTrack,*bestMatch.second);
+      std::pair<bool, double> bestChi2 = 
+	theTrackMatcher->match(*staTrack,*bestMatch.second);
       hChi2->Fill(bestChi2.second);
     }
-  }
-  
+  }  
 }
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(GLBTrackMatchAnalyzer)
+  
